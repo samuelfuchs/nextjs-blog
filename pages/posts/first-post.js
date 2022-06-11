f61f7143dd4fc6df4c1e566859edc2a64d9caf3c
Gerson Samuel Fuchs <fuchsgerson@gmail.com>
2022-06-11 10:41:08 -0300

    layout.module.css styling

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,20 +1,13 @@
-import Link from "next/link"
 import Head from "next/head"
-import Script from "next/script"
+import Link from "next/link"
+import Layout from "../../components/layout"
 
 export default function FirstPost() {
     return (
-    <>
+    <Layout>
     <Head>
         <title>First Post</title>
     </Head>
-    <Script
-        src="https://connect.facebook.net/en_US/sdk.js"
-        strategy="lazyOnload"
-        onLoad={() =>
-          console.log(`script loaded correctly, window.FB has been populated`)
-        }
-      />
     <h1>First Post</h1>
     <h2>
         <Link href="/">
@@ -23,5 +16,5 @@ export default function FirstPost() {
 
     </h2>
     
-    </>)
+    </Layout>)
 }
