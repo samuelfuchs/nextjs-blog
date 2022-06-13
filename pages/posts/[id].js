b9890bbec93632a18064adf4a73d1adc36b97604
Gerson Samuel Fuchs <fuchsgerson@gmail.com>
2022-06-13 15:31:28 -0300

    Polishing the Post Page with Date parser

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,17 +1,29 @@
 import Layout from "../../components/layout";
 // import { getAllPostIds } from "../../lib/posts";
 import { getAllPostIds, getPostData } from "../../lib/posts";
+import Head from "next/head";
+import Date from "../../components/date";
+
+import utilStyles from '../../styles/utils.module.css'
 
 export default function Post({ postData }) {
-    return (<Layout>
-        {postData.title}
-        <br />
-        {postData.id}
-        <br />
-        {postData.date}
-        <br />
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-    </Layout>)
+    return (
+        <Layout>
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+            <article>
+                <h1 className={utilStyles.headingXl}>
+                    {postData.title}
+                </h1>
+                <div className={utilStyles.lightText}>
+                    <Date dateString={postData.date} />
+                </div>
+
+                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            </article>
+        </Layout>
+    )
 }
 
 export async function getStaticPaths() {
