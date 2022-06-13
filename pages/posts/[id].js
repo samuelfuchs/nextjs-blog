5dda048fceff47a252b75dd4a6bb6dc1108d0c3e
Gerson Samuel Fuchs <fuchsgerson@gmail.com>
2022-06-13 15:22:37 -0300

    Render Markdown with remark

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -9,7 +9,8 @@ export default function Post({ postData }) {
         {postData.id}
         <br />
         {postData.date}
-        
+        <br />
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>)
 }
 
@@ -24,7 +25,7 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
     // Fetch necessary data for the blog post using params.id
-    const postData = getPostData(params.id)
+    const postData = await getPostData(params.id)
     return {
         props: {
             postData,
